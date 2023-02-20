95906ebdc7d91a2c460da0d25f35cb899b319081
Ahmad-Fayaz7 <92148518+Ahmad-Fayaz7@users.noreply.github.com>
2023-02-20 13:52:01 +0100

    changes in API link

--- harrypotter.js
+++ harrypotter.js
@@ -2,7 +2,7 @@
 
 class Harrypotter {
   async getCharacters() {
-    const response = await fetch("http://hp-api.herokuapp.com/api/characters");
+    const response = await fetch("https://hp-api.onrender.com/api/characters");
     const charatersData = await response.json();
     return charatersData;
   }
